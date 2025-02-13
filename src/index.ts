7abc59b98d9e51bc454dc93d369dce6892acc74c
ganu <gwbaik9717@icloud.com>
2025-02-13 21:05:42 +0900

    chore: Add beforeload handler to guarantee all necessary paramters exist

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import { $ } from "@/utils/dom";
 import { router } from "@/router";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const app = $("#app");
-  if (!app) {
+  const $app = $("#app");
+  if (!$app) {
     throw new Error("Cannot find app root element");
   }
 
@@ -15,13 +15,25 @@ document.addEventListener("DOMContentLoaded", () => {
       {
         path: "/",
         handler: () => {
-          currentWeatherPage.render(app);
+          currentWeatherPage.render($app);
         },
       },
       {
         path: "/forecast",
         handler: () => {
-          forecastWeatherPage.render(app);
+          forecastWeatherPage.render($app);
+        },
+        beforeLoad: (location) => {
+          const { params } = location;
+          const searchParams = params.searchParams;
+          if (!searchParams) {
+            throw router.navigate("/");
+          }
+          const lat = searchParams.get("lat");
+          const lon = searchParams.get("lon");
+          if (!lat || !lon) {
+            throw router.navigate("/");
+          }
         },
       },
     ],
